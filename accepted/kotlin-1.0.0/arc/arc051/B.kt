97491f2d9a8c0010920dd3a50f6bc89d31fd32a7
Shinjiro Sakaitani <ssakaitani@gmail.com>
2020-03-30 15:20:21 +0900

    :coffee: Solve AtCoder Regular Contest 051
    - B. 互除法

---
+++ accepted/kotlin-1.0.0/arc/arc051/B.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val k = readLine()!!.toInt()
+    val dp = IntArray(50) { 1 }
+    for (i in 0 until k) {
+        dp[i + 2] = dp[i + 1] + dp[i]
+    }
+    println("${dp[k + 1]} ${dp[k]}")
+}
