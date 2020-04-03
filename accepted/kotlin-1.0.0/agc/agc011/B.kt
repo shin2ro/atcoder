30c511ce75bcdf235a7daa1c5196bcfea064fa3b
Shinjiro Sakaitani <ssakaitani@gmail.com>
2020-04-03 14:58:58 +0900

    :coffee: Solve AtCoder Grand Contest 011
    - B. Colorful Creatures

---
+++ accepted/kotlin-1.0.0/agc/agc011/B.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    val a = readLine()!!.split(' ').map { it.toLong() }.sorted()
+    val s = LongArray(n + 1)
+    for (i in 0 until n) {
+        s[i + 1] = s[i] + a[i]
+    }
+
+    var min = 0
+    for (i in 1 until n) {
+        if (s[i] * 2 >= a[i]) continue
+        min = i
+    }
+
+    println(n - min)
+}
