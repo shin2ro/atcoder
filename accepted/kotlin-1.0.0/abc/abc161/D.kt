e5b5e47c37e513a6d8ed1ffec350e957e63dc18d
Shinjiro Sakaitani <ssakaitani@gmail.com>
2020-04-04 23:36:24 +0900

    :coffee: Solve AtCoder Beginner Contest 161
    - D. Lunlun Number

--- accepted/kotlin-1.0.0/abc/abc161/D.kt
+++ accepted/kotlin-1.0.0/abc/abc161/D.kt
@@ -1,35 +1,16 @@
+import java.util.*
+
 fun main(args: Array<String>) {
-    val t = LongArray(100001)
-    var cur = 0
+    val k = readLine()!!.toInt()
+    val queue = ArrayDeque<Long>()
+    for (i in 1..9L) queue.addLast(i)
 
-    fun rec(s: String, f: Boolean) {
-        if (cur == t.size) {
-            return
-        }
-
-        if (s.length == 10) {
-            t[cur] = s.toLong()
-            cur++
-            return
-        }
-
-        if (f) {
-            rec(s + '0', true)
-            for (i in 1..9) {
-                rec(s + ('0' + i), false)
-            }
-        } else {
-            val x = s.last() - '0'
-
-            for (i in -1..1) {
-                if (x + i in 0..9) {
-                    rec(s + ('0' + x + i), false)
-                }
-            }
-        }
+    repeat(k - 1) {
+        val x = queue.removeFirst()
+        if (x % 10 != 0L) queue.addLast(x * 10 + x % 10 - 1)
+        queue.addLast(x * 10 + x % 10)
+        if (x % 10 != 9L) queue.addLast(x * 10 + x % 10 + 1)
     }
 
-    rec("", true)
-
-    println(t[readLine()!!.toInt()])
+    println(queue.removeFirst())
 }
