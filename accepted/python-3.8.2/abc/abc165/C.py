5d534e398378ef4f2c2a8e331438912646d35ff5
Shinjiro Sakaitani <ssakaitani@gmail.com>
2020-05-03 02:52:53 +0900

    :coffee: Solve AtCoder Beginner Contest 165
    - C. Many Requirements

---
+++ accepted/python-3.8.2/abc/abc165/C.py
@@ -0,0 +1,10 @@
+import itertools
+
+N, M, Q = map(int, input().split())
+qs = [list(map(int, input().split())) for _ in range(Q)]
+
+ans = 0
+for A in itertools.combinations_with_replacement(range(1, M + 1), N):
+    ans = max(sum([d for (a, b, c, d) in qs if A[b - 1] - A[a - 1] == c]), ans)
+
+print(ans)
