8c6499ac981d47cdb7a13ce8625150cdd07aee84
Shinjiro Sakaitani <ssakaitani@gmail.com>
2020-06-02 00:29:43 +0900

    :coffee: Solve AtCoder Beginner Contest 169
    - E. Count Median

---
+++ accepted/kotlin-1.3.71/abc/abc169/E.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val n = readLine()!!.toInt()
+    val xs = (0 until n).map {
+        val (a, b) = readLine()!!.split(' ').map { it.toInt() }
+        a to b
+    }
+
+    val ans = if (n % 2 == 0) {
+        val l = xs.map { it.first }.sorted().let { it[n / 2 - 1] + it[n / 2] }
+        val r = xs.map { it.second }.sorted().let { it[n / 2 - 1] + it[n / 2] }
+        r - l + 1
+    } else {
+        val l = xs.map { it.first }.sorted()[n / 2]
+        val r = xs.map { it.second }.sorted()[n / 2]
+        r - l + 1
+    }
+
+    println(ans)
+}
