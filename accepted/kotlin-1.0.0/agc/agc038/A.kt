d54e0a01b801cad9c712475c9e2e4fd1a2d8c463
Shinjiro Sakaitani <ssakaitani@gmail.com>
2020-06-08 21:26:25 +0900

    :coffee: Solve AtCoder Grand Contest 038
    - A. 01 Matrix

---
+++ accepted/kotlin-1.0.0/agc/agc038/A.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val (h, w, a, b) = readLine()!!.split(' ').map { it.toInt() }
+    val s1 = "0".repeat(a) + "1".repeat(w - a)
+    val s2 = "1".repeat(a) + "0".repeat(w - a)
+    val ans = (0 until h).joinToString("\n") { if (it < b) s1 else s2 }
+    println(ans)
+}
