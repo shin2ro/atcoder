c4c0bef76b58496a7a2e939c46d55eee90eae253
Shinjiro Sakaitani <ssakaitani@gmail.com>
2020-06-12 14:41:16 +0900

    :coffee: Solve AtCoder Regular Contest 026
    - A. ダイナミックなポーズ
    - B. 完全数

---
+++ accepted/kotlin-1.0.0/arc/arc026/B.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    val n = readLine()!!.toLong()
+
+    var s = 0L
+    var i = 1L
+    while (i * i <= n) {
+        if (n % i == 0L) {
+            s += i
+            if (i != n / i) s += n / i
+        }
+        i++
+    }
+
+    val ans = when {
+        s == n * 2 -> "Perfect"
+        s < n * 2 -> "Deficient"
+        else -> "Abundant"
+    }
+
+    println(ans)
+}
